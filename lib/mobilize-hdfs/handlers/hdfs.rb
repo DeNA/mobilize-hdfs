52a01778a5034280f8296acaf48f454a701b81e5
Cassio Paes-Leme <cpaesleme@ngmoco.com>
2013-03-20 12:04:21 -0700

    fix include bug

--- lib/mobilize-hdfs/handlers/hdfs.rb
+++ lib/mobilize-hdfs/handlers/hdfs.rb
@@ -52,20 +52,25 @@ module Mobilize
 
     #used for writing strings straight up to hdfs
     def Hdfs.write_by_dataset_path(dst_path,string,user_name)
-      file_hash = {'file.txt'=>string}
       cluster = dst_path.split("/").first
       url = Hdfs.url_by_path(dst_path,user_name)
       hdfs_url = Hdfs.hdfs_url(url)
-      #make sure path is clear
-      delete_command = "dfs -rm '#{hdfs_url}'"
-      Hadoop.run(cluster,delete_command,user_name)
-      write_command = "dfs -copyFromLocal file.txt '#{hdfs_url}'"
-      response = Hadoop.run(cluster,write_command,user_name,file_hash)
+      response = Hdfs.write(cluster,hdfs_url,string,user_name)
       if response['exit_code'] != 0
         raise "Unable to write to #{url} with error: #{response['stderr']}"
       else
         return response
       end
+    end
+
+    def Hdfs.write(cluster,hdfs_url,string,user_name)
+      file_hash = {'file.txt'=>string}
+      #make sure path is clear
+      delete_command = "dfs -rm '#{hdfs_url}'"
+      Hadoop.run(cluster,delete_command,user_name)
+      write_command = "dfs -copyFromLocal file.txt '#{hdfs_url}'"
+      response = Hadoop.run(cluster,write_command,user_name,file_hash)
+      response
     end
 
     #copy file from one url to another
@@ -139,7 +144,8 @@ module Mobilize
       s = Stage.where(:path=>stage_path).first
       u = s.job.runner.user
       user_name = s.params['user']
-      cluster ||= (s.params['cluster'] || Hadoop.default_cluster)
+      cluster ||= s.params['cluster']
+      cluster = Hadoop.default_cluster unless Hadoop.clusters.include?(cluster)
       node = Hadoop.gateway_node(cluster)
       if user_name and !Ssh.sudoers(node).include?(u.name)
         raise "#{u.name} does not have su permissions for node #{node}"
@@ -154,7 +160,6 @@ module Mobilize
       source = s.sources.first
       target = s.target
       cluster = target.url.split("://").last.split("/").first
-      #update stage with the node so we can use it
       user_name = Hdfs.user_name_by_stage_path(stage_path,cluster)
       stdout = if source.handler == 'hdfs'
                    Hdfs.copy(source.url,target.url,user_name)
