2cd2cf26b4d21e07d83e275f6b42ea34edc22bd6
Cassio Paes-Leme <cpaesleme@ngmoco.com>
2013-05-20 11:58:12 -0700

    added home dir

--- lib/mobilize-hdfs.rb
+++ lib/mobilize-hdfs.rb
@@ -3,6 +3,9 @@ require "mobilize-ssh"
 
 module Mobilize
   module Hdfs
+    def Hdfs.home_dir
+      File.expand_path('..',File.dirname(__FILE__))
+    end
   end
 end
 require "mobilize-hdfs/handlers/hadoop"
